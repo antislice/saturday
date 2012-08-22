3fd70a54df9b391460792967d429bc4dac5d7fb4
Georgia A <and.georgia.m@gmail.com>
2012-08-21 23:22:24 -0700

    listed games

--- app/controllers/this_week_controller.rb
+++ app/controllers/this_week_controller.rb
@@ -1,14 +1,17 @@
 class ThisWeekController < ApplicationController
   def game
+  
+  	@games = Game.all
+  	
     home = false
     saturday = false
 
     if home == true and saturday == true
-	@answer = "It's Saturday!"
+			@answer = "It's Saturday!"
     elsif home == true
-	@answer = "Yes!"
+			@answer = "Yes!"
     else
-	@answer = "No."
+			@answer = "No."
     end
   end
 end
