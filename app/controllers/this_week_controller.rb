a97fb6c3fd33cf7ff8459727e4d6fdc54f1f3b6a
Georgia A <and.georgia.m@gmail.com>
2012-08-29 22:01:20 -0700

    hardcoded game in

--- app/controllers/this_week_controller.rb
+++ app/controllers/this_week_controller.rb
@@ -1,16 +1,22 @@
 class ThisWeekController < ApplicationController
   def game
   
-  	@games = Game.all
+#  	@games = Game.all
   	
-  	firstGame = @games.at(@games.index{|g| g.home == true})
+#  	firstGame = @games.at(@games.index{|g| g.home == true})
+		
+		@firstGame = Game.new
+		@firstGame.home = true
+		@firstGame.opponent = "San Jose State University"
+		@firstGame.kickoff = Date.new
 
-    if firstGame.home and Date.today.day == firstGame.kickoff.day
+    if @firstGame.home and Date.today.day == @firstGame.kickoff.day
 			@answer = "It's Saturday!"
-    elsif firstGame.home
+    elsif @firstGame.home
 			@answer = "Yes!"
     else
 			@answer = "No."
     end
+    
   end
 end
