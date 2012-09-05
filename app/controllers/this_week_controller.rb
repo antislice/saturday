20260b0083eb88e5b5cd033399d0062308546e5f
Georgia A <and.georgia.m@gmail.com>
2012-09-04 23:01:30 -0700

    updated for this week's game..

--- app/controllers/this_week_controller.rb
+++ app/controllers/this_week_controller.rb
@@ -7,8 +7,8 @@ class ThisWeekController < ApplicationController
 		
 		@firstGame = Game.new
 		@firstGame.home = true
-		@firstGame.opponent = "San Jose State University"
-		@firstGame.kickoff = DateTime.new(2012, 8, 31, 19, 0, 0)
+		@firstGame.opponent = "Duke"
+		@firstGame.kickoff = DateTime.new(2012, 9, 15, 19, 30, 0)
 
     if @firstGame.home and Date.today.day == @firstGame.kickoff.day
 			@answer = "It's Saturday!"
