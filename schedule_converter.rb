d99b6a58bd000e3ea5359b80fb507f009bea3efd
Georgia A <and.georgia.m@gmail.com>
2013-05-29 22:25:13 -0700

    figured out the date parsing

--- schedule_converter.rb
+++ schedule_converter.rb
@@ -1,6 +1,25 @@
 
 require 'csv'
 
+# header names
+start_date = "START DATE"
+start_time = "START TIME"
+subject = "SUBJECT"
+location = "LOCATION"
+
+first_line = CSV.read('12-outlook-schedule-mfootbl.csv', headers: true).first.to_hash
+puts first_line
+
+puts first_line[subject]
+puts first_line[location]
 
 
-puts CSV.parse('12-outlook-schedule-mfootbl.csv', headers: true).to_json
+
+
+
+
+
+
+
+game_datetime_str = first_line[start_date] << ' ' << first_line[start_time].sub!('PT', 'PST')
+game_datetime = DateTime.strptime(game_datetime_str, '%m/%d/%Y %R %P %Z')
