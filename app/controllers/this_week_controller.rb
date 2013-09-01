6a22f6688d723c8fcc00f83241a40dd07d8a9b32
Georgia A <and.georgia.m@gmail.com>
2013-09-01 00:55:38 -0700

    trying w/o in_time_zone

--- app/controllers/this_week_controller.rb
+++ app/controllers/this_week_controller.rb
@@ -4,14 +4,14 @@ class ThisWeekController < ApplicationController
     games = Game.all
     #saturday = DateTime.new(2012, 9, 17, 3, 0, 0)  	
 
-    @first_game = games.at(games.index{|g| g.game_datetime_local.yday >= Date.today.yday})
+    @first_game = games.at(games.index{|g| g.game_date.yday >= Date.today.yday})
 		
 	#	@firstGame = Game.new
 	#	@firstGame.home = true
 	#	@firstGame.opponent = "U$C (2-0)"
 	#	@firstGame.kickoff = DateTime.new(2012, 9, 15, 16, 30, 0)
     
-    if @first_game.home_or_away == 'home' and Date.today == @first_game.game_datetime_local
+    if @first_game.home_or_away == 'home' and Date.today == @first_game.game_date
 	    @answer = 'It\'s Saturday!'
     elsif @first_game.home_or_away == 'home'
 	    @answer = 'Yes!'
