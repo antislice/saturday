bf42987515f4d84ec6d9ffcfbb8c2dad5fa9c18a
Georgia A <and.georgia.m@gmail.com>
2013-09-07 09:55:54 -0700

    fix y/n message for gameday

--- app/controllers/this_week_controller.rb
+++ app/controllers/this_week_controller.rb
@@ -6,7 +6,7 @@ class ThisWeekController < ApplicationController
 
     @first_game = games.at(games.index{|g| g.game_date.yday >= Date.today.yday})
     
-    if @first_game.home_or_away == 'home' and Date.today == @first_game.game_date
+    if @first_game.home_or_away == 'home' and Date.today.yday == @first_game.game_date.yday
 	    @answer = 'It\'s Saturday!'
     elsif @first_game.home_or_away == 'home'
 	    @answer = 'Yes!'
